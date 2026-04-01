c05072bbaaa099dfc013c2cd13e960a4e64ca557
Elio Fiorentini <efiorent@student.42lyon.fr>
2026-04-01 20:07:12 +0200

    feat: Maze and Cell classes

--- maze/__init__.py
+++ maze/__init__.py
@@ -3,10 +3,12 @@ __author__ = "Nifogi"
 
 from .maze_generator import MazeGenerator
 from .parsing import parse_config_file
+from .maze import Maze
 
 __all__ = [
     "__version__",
     "__author__",
+    "Maze"
     "MazeGenerator",
     "parse_config_file"
 ]
