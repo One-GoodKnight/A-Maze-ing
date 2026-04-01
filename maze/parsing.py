049ebfad02a0266d405e1e3387be19ae277854d9
aginiaux <aginiaux@student.42lyon.fr>
2026-04-01 17:07:30 +0200

    feat: better parsing

--- maze/parsing.py
+++ maze/parsing.py
@@ -1,4 +1,13 @@
 from typing import Dict, Any
+from enum import StrEnum
+
+class Key(StrEnum):
+    WIDTH = 'width'
+    HEIGHT = 'height'
+    ENTRY = 'entry'
+    EXIT = 'exit'
+    OUTPUT_FILE = 'output_file'
+    PERFECT = 'perfect'
 
 def parse_config_file(filename) -> Dict[str, Any]:
     conf = {}
@@ -8,27 +17,39 @@ def parse_config_file(filename) -> Dict[str, Any]:
             line_split = line.split('=')
             if (len(line_split) != 2):
                 raise ValueError("Each line of the config file should have 2 columns separated by an equal")
-            match line_split[0]:
-                case 'WIDTH':
-                    conf['width'] = int(line_split[1])
-                case 'HEIGHT':
-                    conf['height'] = int(line_split[1])
-                case 'ENTRY':
-                    positions = line_split[1].split(',')
+
+            try:
+                key = Key[line_split[0]]
+            except KeyError:
+                raise ValueError(f"Invalid key '{line_split[0]}'. Valid keys are {[key.name for key in Key]}")
+            if (key in conf):
+                raise ValueError(f"Cannot have duplicate keys '{line_split[0]}'")
+            value = line_split[1]
+
+            match key:
+                case 'width' | 'height':
+                    try:
+                        conf[key] = int(value)
+                    except ValueError:
+                        raise ValueError(f"Invalid value for key '{key}', '{value}' should be an int")
+                case 'entry' | 'exit':
+                    positions = value.split(',')
                     if (len(positions) != 2):
-                        raise ValueError("Each entry value should have 2 numbers separated by a comma")
-                    conf['entry'] = (int(positions[0]), positions[1])
-                case 'EXIT':
-                    positions = line_split[1].split(',')
-                    if (len(positions) != 2):
-                        raise ValueError("Each exit value should have 2 numbers separated by a comma")
-                    conf['exit'] = (int(positions[0]), positions[1])
-                case 'OUTPUT_FILE':
-                    conf['output_file'] = line_split[1]
-                case 'PERFECT':
-                    if (line_split[1] != "True" and line_split[1] != "False"):
+                        raise ValueError(f"Each '{key}' value should have 2 ints separated by a comma")
+                    try:
+                        pos = positions[0]
+                        v1 = int(pos)
+                        pos = positions[1]
+                        v2 = int(pos)
+                    except ValueError:
+                        raise ValueError(f"Invalid value for key '{key}', '{pos}' should be an int")
+                    conf[key] = (v1, v2)
+                case 'output_file':
+                    conf[key] = value
+                case 'perfect':
+                    if (value != "True" and value != "False"):
                         raise ValueError("Perfect value should be 'True' or 'False'")
-                    conf['perfect'] = True if line_split[1] == "True" else False
+                    conf[key] = True if value == "True" else False
                 case _:
                     raise ValueError("Unexpected file, accepting only: KEY=VALUE")
     return conf
