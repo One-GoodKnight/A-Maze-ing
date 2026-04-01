28f223e69336a5b3c5b348700ab9bc3e13f6ec74
Elio Fiorentini <efiorent@student.42lyon.fr>
2026-04-01 20:07:12 +0200

    feat: Maze and Cell classes

--- maze/maze.py
+++ maze/maze.py
@@ -1,11 +1,39 @@
 from pydantic import BaseModel, Field, model_validator
 from typing import Self, ClassVar
 
+class Cell(BaseModel):
+    north: bool = Field(default=False)
+    east: bool = Field(default=False)
+    south: bool = Field(default=False)
+    west: bool = Field(default=False)
+
+    @staticmethod
+    def from_hex(hex: str) -> Self:
+        nb = int(hex, 16)
+        north = nb & 1
+        east = nb >> 1 & 1
+        south = nb >> 2 & 1
+        west = nb >> 3 & 1
+        return Cell(north=north, east=east, south=south, west=west)
+
+    def to_hex(self) -> str:
+        north = int(self.north)
+        east = int(self.east) << 1
+        south = int(self.south) << 2
+        west = int(self.west) << 3
+        return f"{(north + east + south + west):X}"
+
+    def __str__(self) -> str:
+        return self.to_hex()
+
+    def __repr__(self) -> str:
+        return self.to_hex()
+
+
 class Maze(BaseModel):
     MAX_SIZE: ClassVar = 100
 
-    maze: list[bytes]
-    maze: list[list[tuple[int, int, int, int]]]
+    maze: list[list[Cell]]
     solution: str
     width: int = Field(ge=1, le=MAX_SIZE)
     height: int = Field(ge=1, le=MAX_SIZE)
@@ -30,14 +58,88 @@ class Maze(BaseModel):
 
     @staticmethod
     def from_file(filename: str) -> Self:
-        maze: list[str] = []
+        maze_str: list[str] = []
         with open(filename, 'r') as f:
-            line = f.readline()
+            line = f.readline().strip()
             width = len(line)
-            while line.strip() != '':
+            while line != '':
                 if len(line) != width:
                     raise ValueError("Invalid file content: "
                                      "inconsistent maze line length")
-                maze.append(line)
-                line = f.readline()
-            line = f.readline()
+                maze_str.append(line)
+                line = f.readline().strip()
+            entry: tuple[int, int] = tuple(f.readline().strip().split(','))
+            exit: tuple[int, int] = tuple(f.readline().strip().split(','))
+            solution = f.readline().strip()
+            maze: list[list[Cell]] = [[Cell.from_hex(x) for x in line] for line in maze_str]
+            return Maze(maze=maze, solution=solution, width=width,
+                        height=len(maze), entry=entry, exit=exit,
+                        output_file=filename)
+
+    def __str__(self) -> str:
+        ret: str = ''
+        for line in self.maze:
+            top: str = ' '
+            bot: str = ' '
+            for byte in line:
+                if byte.to_hex() == '0':
+                    top += '  '
+                    bot += '  '
+                elif byte.to_hex() == '1':
+                    top += '🭶🭶'
+                    bot += '  '
+                elif byte.to_hex() == '2':
+                    top += ' 🭵'
+                    bot += ' 🭵'
+                elif byte.to_hex() == '3':
+                    top += '🭶🭾'
+                    bot += ' 🭵'
+                elif byte.to_hex() == '4':
+                    top += '  '
+                    bot += '🭻🭻'
+                elif byte.to_hex() == '5':
+                    top += '🭶🭶'
+                    bot += '🭻🭻'
+                elif byte.to_hex() == '6':
+                    top += ' 🭵'
+                    bot += '🭻🭿'
+                elif byte.to_hex() == '7':
+                    top += '🭶🭾'
+                    bot += '🭻🭿'
+                elif byte.to_hex() == '8':
+                    top += '🭰 '
+                    bot += '🭰 '
+                elif byte.to_hex() == '9':
+                    top += '🭽🭶'
+                    bot += '🭰 '
+                elif byte.to_hex() == 'A':
+                    top += '🭰🭵'
+                    bot += '🭰🭵'
+                elif byte.to_hex() == 'B':
+                    top += '🭽🭾'
+                    bot += '🭰🭵'
+                elif byte.to_hex() == 'C':
+                    top += '🭰 '
+                    bot += '🭼🭻'
+                elif byte.to_hex() == 'D':
+                    top += '🭽🭶'
+                    bot += '🭼🭻'
+                elif byte.to_hex() == 'E':
+                    top += '🭰🭵'
+                    bot += '🭼🭿'
+                elif byte.to_hex() == 'F':
+                    top += '🭽🭾'
+                    bot += '🭼🭿'
+                else:
+                    ret += byte.to_hex()
+            ret += top + '\n'
+            ret += bot + '\n'
+        return ret
+
+    def __repr__(self) -> str:
+        ret: str = ''
+        for line in self.maze:
+            for byte in line:
+                ret += byte.to_hex()
+            ret += '\n'
+        return ret
