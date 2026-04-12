66dd855bca06d4b50d9cf570defe312be099cdf7
Elio Fiorentini <efiorent@student.42lyon.fr>
2026-04-12 14:38:24 +0200

    perf(font): hugely optimize font rendering

--- display/font.py
+++ display/font.py
@@ -3,32 +3,32 @@ class Font:
     def __init__(self, fontname: str) -> None:
         self.width = 8
         self.height = 14
-        self.letters: dict[str, list[str]] = {}
+        self.chars: dict[str, list[str]] = {}
         with open(fontname, 'r') as f:
             raw_font = f.read()
-        for i, letter in enumerate(range(ord('!'), ord('~') + 1)):
+        for i, char in enumerate(range(ord('!'), ord('~') + 1)):
             char_image: list[str] = []
             for line in raw_font.splitlines():
                 tmp_line: str = ''
-                for char in line[(i * self.width):((i + 1) * self.width)]:
-                    tmp_line += char
+                for pixel in line[(i * self.width):((i + 1) * self.width)]:
+                    tmp_line += pixel
                 char_image.append(tmp_line)
-            self.letters.update({chr(letter): char_image})
+            self.chars.update({chr(char): char_image})
 
     def __getitem__(self, index: str) -> list[str]:
-        return self.letters[index]
+        return self.chars[index]
 
     def __str__(self, char: str) -> str:
         s: str = ''
-        for letter in self.letters:
-            for line in letter:
+        for char in self.chars:
+            for line in char:
                 s += line + '\n'
         return s
 
     def __repr__(self) -> str:
         s: str = '{\n'
-        for letter in self.letters:
-            for line in letter:
+        for char in self.chars:
+            for line in char:
                 s += repr(line) + '\n'
         s += '}'
         return s
--- display/image.py
+++ display/image.py
@@ -49,11 +49,10 @@ class Image():
         y0 = min(self.height - 1, max(0, start[1]))
         x1 = min(self.width - 1, max(0, end[0]))
         y1 = min(self.height - 1, max(0, end[1]))
-        bytes_pp = self.bytes_pp
         color = self.endian_color(argb)
         self.data[
             y0: y1,
-            x0*bytes_pp: x1*bytes_pp
+            x0 * self.bytes_pp: x1 * self.bytes_pp
         ] = np.tile(color, x1 - x0)
 
     def set_to(self, color: int) -> None:
@@ -82,58 +81,31 @@ class Image():
     def set_font(self, font: Font) -> None:
         self.font = font
 
-    def print_char(self, x: int, y: int,
-                   char: str, **kwargs) -> None:
-        font = self.font
-        color = self.endian_color(kwargs.get('color', BLACK))
-        bg_color = self.endian_color(kwargs.get('bg_color', None))
-        size = kwargs.get('size', 1)
-        if size <= 0:
-            size = 1
-        letter = font.letters.get(char)
-        if letter is None:
-            return
-        for i in range(font.width * size):
-            for j in range(font.height * size):
-                x_coord = (x + i) * self.bytes_pp
-                y_coord = y + j
-                if (y_coord >= 0 and y_coord < self.height and x_coord >= 0
-                        and x_coord < self.width * self.bytes_pp):
-                    if letter[j // size][i // size] == '#':
-                        if color is not None:
-                            self.data[
-                                y_coord,
-                                x_coord: x_coord + self.bytes_pp
-                            ] = color
-                    else:
-                        if bg_color is not None:
-                            self.data[
-                                y_coord,
-                                x_coord: x_coord + self.bytes_pp
-                            ] = bg_color
-
-    def print(self, x: int, y: int, text: str, **kwargs) -> None:
-        if not text.isascii():
-            raise ValueError(f"Invalid text: '{text}' "
-                             "contains non ASCII characters")
+    def print_char(self, x: int, y: int, char: str, **kwargs) -> None:
         font = self.font
         color = kwargs.get('color', BLACK)
+        size = max(kwargs.get('size', 1), 1)
+        char = font.chars.get(char)
+        if char is None:
+            return
+        for i in range(font.width):
+            for j in range(font.height):
+                if char[j][i] == '#' and color is not None:
+                    start = (x + i * size, y + j * size)
+                    end = (start[0] + size, start[1] + size)
+                    self.draw_rect(start, end, color)
+
+    def print(self, x: int, y: int, string: str, **kwargs) -> None:
+        font = self.font
         bg_color = kwargs.get('bg_color', None)
-        size = kwargs.get('size', 1)
-        if size <= 0:
-            size = 1
-        if x == -1:
-            x = (self.width // 2) - (len(text) * font.width * size // 2)
-        if y == -1:
-            y = (self.height // 2) - (font.height * size // 2)
-        for i, char in enumerate(text):
-            offset = i * font.width * size
-            if char == ' ' and bg_color is not None:
-                self.draw_rect(
-                    (x + offset, y),
-                    (x + offset + (font.width * size), y + font.height * size),
-                    bg_color
-                )
-            else:
-                self.print_char(x + offset, y, char, color=color,
-                                bg_color=bg_color, size=size)
+        size = max(kwargs.get('size', 1), 1)
+        char_width, char_height = (font.width * size, font.height * size)
+        bg_width = len(string) * char_width
+        x = x if x >= 0 else (self.width // 2) - (bg_width // 2)
+        y = y if y >= 0 else (self.height // 2) - (char_height // 2)
+        if bg_color is not None:
+            self.draw_rect((x, y), (x + bg_width, y + char_height), bg_color)
+        offset = 0
+        for char in string:
+            self.print_char(x + offset, y, char, **kwargs)
+            offset += char_width
